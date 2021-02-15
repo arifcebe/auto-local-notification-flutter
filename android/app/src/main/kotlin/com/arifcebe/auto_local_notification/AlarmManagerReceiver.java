8fe5fcb1d468d354d31e7e87e90c78c41c736a28
lukmansp <lukmanssefriyanto@gmail.com>
2021-02-15 15:21:27 +0700

    add auto start and open app when user press notification

--- android/app/src/main/kotlin/com/arifcebe/auto_local_notification/AlarmManagerReceiver.java
+++ android/app/src/main/kotlin/com/arifcebe/auto_local_notification/AlarmManagerReceiver.java
@@ -2,6 +2,7 @@ package com.arifcebe.auto_local_notification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,6 +16,12 @@ public class AlarmManagerReceiver  extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         /// TODO show notification when time has come
+        Intent myIntent = new Intent(context,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                myIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder builder;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             builder = new NotificationCompat.Builder(context,"Azan");
@@ -29,7 +36,8 @@ public class AlarmManagerReceiver  extends BroadcastReceiver {
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Alarm")
                 .setContentText("Waktunya sholat")
-                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND);
+                .setContentIntent(pendingIntent)
+                .setDefaults(Notification.DEFAULT_SOUND);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(5,builder.build());
--- android/app/src/main/kotlin/com/arifcebe/auto_local_notification/MainActivity.java
+++ android/app/src/main/kotlin/com/arifcebe/auto_local_notification/MainActivity.java
@@ -1,10 +1,13 @@
 package com.arifcebe.auto_local_notification;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -31,6 +34,7 @@ public class MainActivity extends FlutterActivity {
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
+
         MethodChannel methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),CHANNEL_NAME);
         methodChannel.setMethodCallHandler((call, result) -> {
             if (call.method.equals("startAlarm")){
@@ -38,6 +42,7 @@ public class MainActivity extends FlutterActivity {
                 Log.d("method_channel","run set alarm");
                 setAlarmManager();
                 result.success(true);
+                enableAutoStart();
             }
         });
     }
@@ -66,4 +71,101 @@ public class MainActivity extends FlutterActivity {
         pendingIntent = PendingIntent.getBroadcast(this,1,intent,0);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),1000 * 60,pendingIntent);
     }
+    private void enableAutoStart() {
+        Log.d("check devices","devicesnya "+Build.BRAND);
+        if (Build.BRAND.equalsIgnoreCase("Xiaomi")) {
+            Log.d("xiaomi devices","AutoStart need authenticate");
+            new AlertDialog.Builder(MainActivity.this).setTitle("Enable AutoStart")
+                    .setMessage(
+                            "Please allow AppName to always run in the background,else our services can't be accessed.")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Intent intent = new Intent();
+                            intent.setComponent(new ComponentName("com.miui.securitycenter",
+                                    "com.miui.permcenter.autostart.AutoStartManagementActivity"));
+                            startActivity(intent);
+                        }
+                    }).show();
+        }else if (Build.BRAND.equalsIgnoreCase("Redmi")) {
+            Log.d("xiaomi devices","AutoStart need authenticate");
+            new AlertDialog.Builder(MainActivity.this).setTitle("Enable AutoStart")
+                    .setMessage(
+                            "Please allow AppName to always run in the background,else our services can't be accessed.")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Intent intent = new Intent();
+                            intent.setComponent(new ComponentName("com.miui.securitycenter",
+                                    "com.miui.permcenter.autostart.AutoStartManagementActivity"));
+                            startActivity(intent);
+                        }
+                    }).show();
+        }else if (Build.MANUFACTURER.equalsIgnoreCase("oppo")) {
+            new AlertDialog.Builder(MainActivity.this).setTitle("Enable AutoStart")
+                    .setMessage(
+                            "Please allow AppName to always run in the background,else our services can't be accessed.")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            try {
+                                Intent intent = new Intent();
+                                intent.setClassName("com.coloros.safecenter",
+                                        "com.coloros.safecenter.permission.startup.StartupAppListActivity");
+                                startActivity(intent);
+                            } catch (Exception e) {
+                                try {
+                                    Intent intent = new Intent();
+                                    intent.setClassName("com.oppo.safe",
+                                            "com.oppo.safe.permission.startup.StartupAppListActivity");
+                                    startActivity(intent);
+                                } catch (Exception ex) {
+                                    try {
+                                        Intent intent = new Intent();
+                                        intent.setClassName("com.coloros.safecenter",
+                                                "com.coloros.safecenter.startupapp.StartupAppListActivity");
+                                        startActivity(intent);
+                                    } catch (Exception exx) {
+
+                                    }
+                                }
+                            }
+                        }
+                    }).show();
+        } else if (Build.MANUFACTURER.contains("vivo")) {
+            new AlertDialog.Builder(MainActivity.this).setTitle("Enable AutoStart")
+                    .setMessage(
+                            "Please allow AppName to always run in the background.Our app runs in background else our services can't be accesed.")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            try {
+                                Intent intent = new Intent();
+                                intent.setComponent(new ComponentName("com.iqoo.secure",
+                                        "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity"));
+                                startActivity(intent);
+                            } catch (Exception e) {
+                                try {
+                                    Intent intent = new Intent();
+                                    intent.setComponent(new ComponentName("com.vivo.permissionmanager",
+                                            "com.vivo.permissionmanager.activity.BgStartUpManagerActivity"));
+                                    startActivity(intent);
+                                } catch (Exception ex) {
+                                    try {
+                                        Intent intent = new Intent();
+                                        intent.setClassName("com.iqoo.secure",
+                                                "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager");
+                                        startActivity(intent);
+                                    } catch (Exception exx) {
+                                        ex.printStackTrace();
+                                    }
+                                }
+                            }
+                        }
+                    }).show();
+        }
+    }
+
 }
